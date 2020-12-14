3f25005fa8ba9b4675be8f66d9ab9138436b3133
Jacob Lee <jlee93115@163.com>
2020-12-13 23:11:43 -0800

    Add login page

---
+++ client/paper-trader/src/components/pages/Login.vue
@@ -0,0 +1,24 @@
+<template>
+  <a-form :model="form">
+      <a-form-item label="Activity name">
+      <a-input/>
+    </a-form-item>
+  </a-form>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Login',
+  components: {
+  },
+  props: {
+        
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
