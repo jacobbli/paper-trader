41625e72f8db0f8b1ac0b34e8d84b8fa5451c5f0
Jacob Lee <jlee93115@163.com>
2020-12-25 15:33:00 -0800

    Add authentication methods and states

--- client/paper-trader/src/store/modules/users.js
+++ client/paper-trader/src/store/modules/users.js
@@ -1,28 +1,66 @@
 export default {
     state: {
-        isLoggedIn: String,
-        username: String,
-        firstName: String,
-        lastName: String,
-        funds: Number,
-        access_token: String
+        username: null,
+        firstName: null,
+        lastName: null,
+        funds: null,
+        accessToken: sessionStorage.getItem('access-token')
     },
 
     getters: {
-        accessToken: state => {
-            return state.access_token
-        }
+        accessToken: state => state.accessToken,
+        isLoggedIn : state => !!state.accessToken
     },
 
     mutations: {
         SET_ACCESS_TOKEN(state, token) {
-            state.access_token = token;
+            state.accessToken = token;
+        },
+        SET_USERNAME(state, username) {
+            state.username = username;
+        },
+        SET_FIRSTNAME(state, firstName) {
+            state.firstName = firstName;
+        },
+        SET_LASTNAME(state, lastName) {
+            state.lastName = lastName;
+        },
+        SET_FUNDS(state, funds) {
+            state.funds = funds;
         }
     },
 
     actions: {
         setAccessToken(context, token) {
+            sessionStorage.setItem('access-token', token)
             context.commit('SET_ACCESS_TOKEN', token)
+        },
+        setUsername(context, username) {
+            context.commit('SET_USERNAME', username)
+        },
+        setFirstName(context, firstName) {
+            context.commit('SET_FIRSTNAME', firstName)
+        },
+        setLastName(context, lastName) {
+            context.commit('SET_LASTNAME', lastName)
+        },
+        setFunds(context, funds) {
+            context.commit('SET_FUNDS', funds)
+        },
+        setCurrentUser(context, payload) {
+            context.dispatch('setAccessToken', payload['access_token']);
+            context.dispatch('setUsername', payload['username']);
+            context.dispatch('setFirstName', payload['first_name']);
+            context.dispatch('setLastName', payload['last_name']);
+            context.dispatch('setFunds', payload['funds']);
+        },
+        logout(context) {
+            sessionStorage.removeItem('access-token')
+            context.dispatch('setAccessToken', null);
+            context.dispatch('setUsername', null);
+            context.dispatch('setFirstName', null);
+            context.dispatch('setLastName', null);
+            context.dispatch('setFunds', null);
         }
     }
 };
