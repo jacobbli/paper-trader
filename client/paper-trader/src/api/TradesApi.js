0ffb9593feb5226ee641fbba6a5e6bc90b466d90
Jacob Lee <jlee93115@163.com>
2021-01-05 00:17:18 -0800

    Check for refresh token on failed access token authentication

--- client/paper-trader/src/api/TradesApi.js
+++ client/paper-trader/src/api/TradesApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import store from '../store/store.js'
-import { logout } from '../api/UsersApi.js'
+import { forceLogout, requestTokens } from '../api/UsersApi.js'
 
 
 export function getOwnedStocks() {
@@ -10,19 +10,44 @@ export function getOwnedStocks() {
     headers: {
       'Authorization': `Bearer ${store.getters.getAccessToken}`
     }
-  }).then(result => {
-    store.dispatch('getOwnedStocks', result.data)
-  }, error => {
-    console.error(error);
-    if (error.response.status == 401) {
-      logout()
+  }).then(
+    result => {
+      store.dispatch('getOwnedStocks', result.data)
+    }, 
+    error => {
+      if (error.response.status == 401) {
+        requestTokens()
+        .then(
+          () => {
+            axios({
+              method: 'GET',
+              url: `${process.env.VUE_APP_API_URL}/trades/owned-securities`,
+              headers: {
+                'Authorization': `Bearer ${store.getters.getAccessToken}`
+              }
+            }).then(
+              result=> {
+                store.dispatch('getOwnedStocks', result.data)
+              },
+              () => forceLogout()
+            )
+          }, 
+          error => {
+            if (error.response.status == 401) {
+              forceLogout()
+            }
+          }
+        )
+      } else {
+        forceLogout()
+      }
     }
-  })
+  )
 }
 
 
 export function buySecurity(orderForm){
-  return new Promise( (resolve, reject) => {
+  return new Promise((resolve, reject) => {
     axios({
       method: 'POST',
       url: `${process.env.VUE_APP_API_URL}/trades/owned-securities`,
@@ -35,18 +60,49 @@ export function buySecurity(orderForm){
           'quantity': orderForm.get('quantity'),
           'price': orderForm.get('price')
         }
-    }).then(() => {
-      store.dispatch('addOwnedSecurity', orderForm) 
-      resolve()
-    }, error => {
-      console.error(error);
-      if (error.response.status == 401) {
-        logout()
+    }).then(
+      () => {
+        store.dispatch('addOwnedSecurity', orderForm) 
+        return resolve('Order successful')
+      },
+      error => {
+        if (error.response.status == 401) {
+          requestTokens()
+          .then(
+            () => {
+              axios({
+                method: 'POST',
+                url: `${process.env.VUE_APP_API_URL}/trades/owned-securities`,
+                headers: {
+                  'Authorization': `Bearer ${store.getters.getAccessToken}`
+                }
+              }).then(
+                result=> {
+                  store.dispatch('addOwnedSecurity', result.data)
+                  return resolve('Order successful')
+                },
+                () => {
+                  forceLogout()
+                  return reject('User is not authenticated')
+                }
+              )
+            }, 
+            error => {
+              if (error.response.status == 401) {
+                forceLogout()
+                return reject('User is not authenticated')
+              }
+            }
+          )
+        } else {
+          forceLogout()
+          return reject('Order failed')
+        }
       }
-      reject()
-    })
+    )
   })
 }
+
 
 export function sellSecurity(orderForm){
   return new Promise((resolve, reject) => {
@@ -63,15 +119,45 @@ export function sellSecurity(orderForm){
           'price': orderForm.get('price')
         }
       }
-    ).then(() => {
-      store.dispatch('removeOwnedSecurity', orderForm)
-      resolve()
-    }, error => {
-      console.error(error);
-      if (error.response.status == 401) {
-        logout()
+    ).then(
+      () => {
+        store.dispatch('removeOwnedSecurity', orderForm)
+        resolve()
+      }, 
+      error => {
+        if (error.response.status == 401) {
+          requestTokens()
+          .then(
+            () => {
+              axios({
+                method: 'DELETE',
+                url: `${process.env.VUE_APP_API_URL}/trades/owned-securities`,
+                headers: {
+                  'Authorization': `Bearer ${store.getters.getAccessToken}`
+                }
+              }).then(
+                result=> {
+                  store.dispatch('removeOwnedSecurity', result.data)
+                  return resolve('Order successful')
+                },
+                () => {
+                  forceLogout()
+                  return reject('User is not authenticated')
+                }
+              )
+            }, 
+            error => {
+              if (error.response.status == 401) {
+                forceLogout()
+                return reject('User is not authenticated')
+              }
+            }
+          )
+        } else {
+          forceLogout()
+          return reject('Order failed')
+        }
       }
-      reject()
-    })
+    )
   })
 }
--- client/paper-trader/src/api/UsersApi.js
+++ client/paper-trader/src/api/UsersApi.js
@@ -8,7 +8,8 @@ export function login(login_form) {
       method: "POST", 
       url: `${process.env.VUE_APP_API_URL}/users/token`,
       data: login_form,
-      headers: {'Content-Type': 'multipart/form-data' }
+      headers: {'Content-Type': 'multipart/form-data' },
+      withCredentials: true
     }).then(result => {
       store.dispatch('login', result.data).then(() =>
         router.push('/dashboard')
@@ -20,7 +21,7 @@ export function login(login_form) {
 
 
 export function isAuthenticated() {
-  return new Promise( (resolve, reject) => {
+  return new Promise((resolve, reject) => {
     if (store.getters.getAccessToken !== null) {
       axios({
         method: 'GET',
@@ -28,24 +29,97 @@ export function isAuthenticated() {
         headers: {
           'Authorization': `Bearer ${store.getters.getAccessToken}`
         }
-      }).then(result => {
-        store.dispatch('authenticate', result.data);
-        resolve();
-      },
-      () => {
-        store.dispatch('logout');
-        reject();
-      })
+      }).then(
+        result => {
+          store.dispatch('authenticate', result.data);
+          resolve('User is authenticated');
+        },
+        error => {
+          if (error.response.status == 401) {
+            requestTokens().then(
+              () => {
+                axios({
+                  method: 'GET',
+                  url:`${process.env.VUE_APP_API_URL}/users/authenticated`,
+                  headers: {
+                    'Authorization': `Bearer ${store.getters.getAccessToken}`
+                  }
+                }).then(
+                  result => {
+                    store.dispatch('authenticate', result.data);
+                    resolve('User is authenticated');
+                  },
+                  () => {
+                    reject('User is not authenticated')
+                  })
+              },
+              () => {
+                reject('User is not authenticated')
+              }
+            )
+          } else {
+            reject('User is not authenticated')
+          }
+        }
+      )
     } else {
-      store.dispatch('logout');
-      reject();
+      requestTokens()
+        .then(
+          () => {
+            axios({
+              method: 'GET',
+              url:`${process.env.VUE_APP_API_URL}/users/authenticated`,
+              withCredentials: true
+            }).then(
+              () => {
+                resolve('User is authenticated');
+              },
+              () => {
+                reject('User is not authenticated');
+              }
+            )
+          },
+          () => {
+            reject('User is not authenticated')
+          }
+        )
+      }
     }
-  })
+  )
 }
 
 
 export function logout() {
   store.dispatch('logout').then(() =>
     router.push('/login')
+  )
+}
+
+
+export function forceLogout() {
+  store.dispatch('logout').then(() =>
+    router.push('/login')
+  )
+}
+
+
+export function requestTokens() {
+  return new Promise((resolve, reject) => {
+    axios({
+      method: "POST", 
+      url: `${process.env.VUE_APP_API_URL}/users/refresh-token`,
+      withCredentials: true
+    })
+      .then(
+        result => {
+          store.dispatch('login', result.data).then(() => 
+            resolve('Obtained new tokens')
+          )
+        }, 
+        () => {
+          reject('Authentication failed')
+        }
+      )
+    }
   )
 }
